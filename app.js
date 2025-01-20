72a5af6ce41451a9363305c777e5035ae4e86d02
rustinayush <ayushsrivastava16252@gmail.com>
2025-01-20 15:56:08 +0530

    1

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; // Corrected the import here
 
-const parent = React.createElement("h1", { id: "heading" }, "Hello Om!");
+// const parent = React.createElement("h1", { id: "heading" }, "Hello Om!");
+
+const jsxparent = <h1 id="heading">Hi Ayush & Om 🎄🎈</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(jsxparent);
